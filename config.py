dfc3a79f949ee15ab24a4c008d30fb87bdf7fb45
lmhssgomathi0212-cloud <lmhssgomathi0212@gmail.com>
2026-09-01 10:42:40 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,9 @@
+APP_NAME = "Agri Equipment Rental"
+APP_ICON = "🚜"
+APP_DESCRIPTION = (
+    "Farmers can easily find and rent "
+    "agricultural equipment."
+)
+CURRENCY = "₹"
+OWNER_ROLE = "Equipment Owner"
+FARMER_ROLE = "Farmer"
