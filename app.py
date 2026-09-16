23878e0c0b96a318220396dced5fff740717528f
lmhssgomathi0212-cloud <lmhssgomathi0212@gmail.com>
2026-09-16 13:13:30 +0530

    Update app.py

--- app.py
+++ app.py
@@ -144,3 +144,39 @@ def show_register():
             st.warning(
                 "Please fill all details."
             )
+import streamlit as st
+from backend.auth import login_user
+
+
+def show_login():
+
+    st.header("🔐 Login")
+
+    email = st.text_input("Email")
+
+    password = st.text_input(
+        "Password",
+        type="password"
+    )
+
+    if st.button("Login"):
+
+        user = login_user(
+            email,
+            password
+        )
+
+        if user:
+
+            st.session_state.logged_in = True
+            st.session_state.username = user[1]
+
+            st.success(
+                f"✅ Welcome {user[1]}!"
+            )
+
+        else:
+
+            st.error(
+                "❌ Invalid email or password."
+            )
